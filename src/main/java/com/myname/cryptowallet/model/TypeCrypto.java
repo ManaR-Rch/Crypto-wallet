28370280face404d23cd370be69e5e448940cf94
ManaR-Rch <manarmarchou6@gmail.com>
2025-09-30 15:02:01 +0100

    adding type crypto enum

---
+++ src/main/java/com/myname/cryptowallet/model/TypeCrypto.java
@@ -0,0 +1,14 @@
+package com.myname.cryptowallet.model;
+
+/**
+ * Types de crypto-monnaies supportés par le simulateur.
+ */
+public enum TypeCrypto {
+    /** Bitcoin (BTC) */
+    BITCOIN,
+    /** Ethereum (ETH) */
+    ETHEREUM
+}
+
+
+
